c69ce35d8ea9f887a7ba3e73a5a8e177a79db2cb
Luis Pintado <luchopintado@gmail.com>
2017-07-20 18:55:23 -0500

    Initial commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
+import YTSearch from 'youtube-api-search';
+
+const API_KEY = 'AIzaSyCj1-izLdJDhcpBVDIncbSySy4ZeiQ0bc8';
 
 export default class App extends Component {
+
+  constructor (props) {
+      super(props);
+
+      this.state = {videos:[]};
+
+      YTSearch({key: API_KEY, term: 'lol cats'}, videos => {
+          this.setState({videos});
+      });
+  }
+
   render() {
-    return (
-      <div>React simple starter</div>
-    );
+      return (
+        <div>
+          <SearchBar />
+          <VideoDetail video={this.state.videos[0]} />
+          <VideoList videos={this.state.videos} />
+        </div>
+      );
   }
 }
